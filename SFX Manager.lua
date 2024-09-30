1eae77a72960727db8b98c51ab660e542584b4bc
Tyler O <101290005+Toakley683@users.noreply.github.com>
2024-09-30 02:41:27 +0100

    Update SFX Manager.lua

--- SFX Manager.lua
+++ SFX Manager.lua
@@ -1,53 +1,49 @@
 --@name Sound Manager
 --@author toakley682
---@shared
+--@client
 
-if CLIENT then
+SoundManager = class( "SoundManager" )
+
+function SoundManager:initialize()
+     
+    self.Settings = "3D noblock"
     
-    SoundManager = class( "SoundManager" )
+    self.UninitializedSongs = {}
+    self.Songs = {}
     
-    function SoundManager:initialize()
-         
-        self.Settings = "3D noblock"
-        
-        self.UninitializedSongs = {}
-        self.Songs = {}
-        
-        self.SongRequests = {}
-        
-    end
+    self.SongRequests = {}
     
-    function SoundManager:addSound( Name, URL )
-        
-        self.UninitializedSongs[ #self.UninitializedSongs + 1 ] = { Name=Name, URL=URL }
-        
-    end
+end
+
+function SoundManager:addSound( Name, URL )
     
-    function SoundManager:Setup()
+    self.UninitializedSongs[ #self.UninitializedSongs + 1 ] = { Name=Name, URL=URL }
+    
+end
+
+function SoundManager:Setup()
+    
+    for Index, SongData in ipairs( self.UninitializedSongs ) do
         
-        for Index, SongData in ipairs( self.UninitializedSongs ) do
+        bass.loadURL( SongData.URL, self.Settings, function( Bass, Err, ErrCode )
             
-            bass.loadURL( SongData.URL, self.Settings, function( Bass, Err, ErrCode )
-                
-                if ErrCode != "" then printConsole( player():getName() .. " could not load sound " .. SongData.Name .. " - Error : " .. ErrCode ) return end
-                
-                self.Songs[ SongData.Name ] = Bass
-                table.remove( self.UninitializedSongs, Index )
-                
-                hook.run( "LoadedSound:"..SongData.Name, Bass )
-                
-            end)
+            if ErrCode != "" then printConsole( player():getName() .. " could not load sound " .. SongData.Name .. " - Error : " .. ErrCode ) return end
             
-        end
+            self.Songs[ SongData.Name ] = Bass
+            table.remove( self.UninitializedSongs, Index )
+            
+            hook.run( "LoadedSound:"..SongData.Name, Bass )
+            
+        end)
         
     end
     
-    function SoundManager:getBass( Name, Callback )
-        
-        if not self.Songs[ Name ] then printConsole( "Song " .. Name .. " does not exist!" ) return end
-        
-        Callback( self.Songs[ Name ] )
-        
-    end
+end
+
+function SoundManager:getBass( Name, Callback )
+    
+    if not self.Songs[ Name ] then printConsole( "Song " .. Name .. " does not exist!" ) return end
+    
+    Callback( self.Songs[ Name ] )
     
 end
